b85c6269e60a9ce8e812aef4be37a86f1db9e392
Laurel Butler <laurelbutler@Laurels-MBP.attlocal.net>
2019-04-17 12:44:40 -0400

    passing test for JWT

--- test/setup.js
+++ test/setup.js
@@ -1,10 +1,11 @@
 process.env.TZ = 'UCT'
 process.env.NODE_ENV = 'test'
+process.env.JWT_SECRET = 'test-jwt-secret'
 
 require('dotenv').config()
 
 process.env.TEST_DB_URL = process.env.TEST_DB_URL
-  || "postgresql://dunder-mifflin@localhost/blogful-auth-test"
+  || "postgresql://dunder-mifflin@localhost/blogfultest"
 
 const { expect } = require('chai')
 const supertest = require('supertest')
